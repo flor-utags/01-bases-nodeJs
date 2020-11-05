e23fcb4a5a58db70d05ce3285419a17e0bc091aa
flor.rangel <flor.rangel@hotmail.com>
2020-11-04 21:42:26 -0600

    Primer commit del proyecto con la multiplicacion completa

---
+++ app.js
@@ -0,0 +1,52 @@
+const argv = require('yargs')
+    .command('listar', 'Imprime en pantalla la tabla de multiplicar', {
+    base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10             
+        } 
+    }) 
+    .command('crear', 'Crea un archivo con la tabla de multiplicar', {
+        base: {
+                demand: true,
+                alias: 'b'
+            },
+            limite: {
+                alias: 'l',
+                default: 10             
+            } 
+        }) 
+    .help()
+    .argv; 
+const multiplicar = require('./Multiplicacion/multiplicacion');
+
+let comando = argv._[0];
+
+let base = argv.base;
+let limite = argv.limite;
+
+switch( comando ) {
+    case 'listar':
+      multiplicar
+        .listarTabla(base, limite)
+        .then() 
+        .catch(error => console.log(`Ocurrio un error ${error}`));
+        break;
+    case 'crear':   
+    multiplicar
+      .crearArchivo(base, limite) 
+      .then(archivo => console.log(`El archivo ${archivo} ha sido generado con exito`)) 
+      .catch(error => console.log(`Ocurrio un error ${error}`));        
+        break;
+    default:
+        console.log('Comando no reconocido');
+} 
+
+multiplicar
+      .crearArchivo(10)
+      .then(archivo => console.log(`El archivo ${archivo} ha sido generado con exito`)) 
+      .catch(error => console.log(`Ocurrio un error ${error}`)); 
+
